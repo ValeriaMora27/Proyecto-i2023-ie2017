3e83b0fcdc843018a7258ed4d428d4f04a024f8c
sergiogarino <sgarino22@gmail.com>
2023-06-25 17:33:18 -0600

    Elimino el directorio de build

--- Proyecto/ProyectoEventos/build/CMakeFiles/ProyectoEventos.dir/ProyectoEventos_autogen/EWIEGA46WW/qrc_imagenes.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/ProyectoEventos.dir/ProyectoEventos_autogen/EWIEGA46WW/qrc_imagenes.cpp.obj: \
- C:/Users/Carlos\ Mora/Documents/Proyecto/Proyecto-i2023-ie2017/Proyecto/ProyectoEventos/build/ProyectoEventos_autogen/EWIEGA46WW/qrc_imagenes.cpp
